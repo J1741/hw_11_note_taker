3095d3cdda65e8104cd7c35ddbafb6af3983b160
J1741 <84889065+J1741@users.noreply.github.com>
2021-09-20 04:42:24 -0400

    setting up server and adding html get route for landing page

---
+++ server.js
@@ -0,0 +1,23 @@
+// setup
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001
+
+const app = express();
+
+// middleware for parsing JSON and urlencoded form data 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// html route for landing page
+app.get('/', (req, res) => {
+  console.log('\n**** Route hit ****\n', req.route);
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`################ App now listening at http://localhost:${PORT} 🚀 ################`)
+);

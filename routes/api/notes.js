4c23fdec6562e3f220050b4790746ba5bf7fe57c
J1741 <84889065+J1741@users.noreply.github.com>
2021-09-22 01:00:05 -0400

    installing a package to generate uuids, adding a post route (not working yet)

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
-const db = require('../../db/db.json');
+const fs = require('fs');
+// const db = require('../../db/db.json');
+const { v4 : uuidv4 } = require('uuid');
+
+const generateUuid = () => uuidv4();
 
 // GET route for retrieving all notes
 router.get('/', (req, res) => {
-  console.log('\n**** Route hit for notes API ****\n', req.route);
-  res.status(200).json(db);
+  console.log('\n**** GET route hit for notes API ****\n', req.route);
+  // res.status(200).json(db);
 })
 
 // POST route for new note
-// to-be-added
+router.post('/', async (req, res) => {
+  console.log('\n**** POST route hit for notes API ****\n', req.route);
+
+  let newNote = req.body;
+  newNote.id = generateUuid();
+
+  await fs.readFile('../../db/db.json', (error, data) => error ? console.error(error) : console.log(data));
+  // noteList.push(newNote);
+
+  // await fs.writeFile('../../db/db.json', JSON.stringify(noteList), (error) => error ? console.error(error) : console.log('Success!'));
+
+  res.send('Sucessfully wrote file!');
+
+})
 
 module.exports = router;
